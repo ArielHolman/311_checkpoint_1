ac68fc5c417ff1d155c177fba29a0601d0374a8e
ArielHolman <ArielARodrigues01@gmail.com>
2020-09-07 19:27:32 -0500

    finished all routes and controllers.

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,5 @@
 const users = require('./../data/index')
-// let counter = users.length+1
-
-// The end result of your routes should look like this: `router.get('/users', usersController.listUsers)`.
+let counter = users.length+1
 
 // * listUsers
 //   * Should retrieve the entire array from _data/index_
@@ -12,20 +10,48 @@ const listUsers = (req, res) => {
 // * showUser
 //   * Should retrieve just the user that matches the passed-in id
 const listUsersById = (req, res) => {
-  let foundUser = users.find( user => user.id === req.params.userId)
+  let foundUser = users.find( user => user.id === parseInt(req.params.id))
   res.json(foundUser)
 }
 
 // * createUser
 //   * Should add a user to the array
-
+const createNewUser = (req, res) => {
+  users.push({
+    id: counter++, ...req.body})
+    res.json(users[users.length-1])
+}
 // * updateUser
 //   * Should update one user in the array based on its id
+const updateUser = (req, res) => {
+  let foundUser = users.find( user => user.id === parseInt(req.params.id))
+  console.log(foundUser)
+  foundUser.name = req.body.name ? req.body.name : foundUser.name
+  foundUser.username = req.body.username ? req.body.username : foundUser.username
+  foundUser.email = req.body.email ? req.body.email : foundUser.email
+  foundUser.address = req.body.address ? req.body.address : foundUser.address
+  foundUser.phone = req.body.phone ? req.body.phone : foundUser.phone
+  foundUser.website = req.body.website ? req.body.website : foundUser.website
+  foundUser.company = req.body.company ? req.body.company : foundUser.company
+  res.json(foundUser)
+}
 
 // * deleteUser
 //   * Should delete one user from the array based on its id
+const deleteUser = (req, res) => {
+  let foundUser = users.find(user => user.id === parseInt(req.params.id))
+  if (foundUser) {
+    foundUser.isActive = false
+    res.send('User unactivated!')
+  } else {
+    res.status(400).json({message: `No comment with the id of ${req.params.id}`})
+  }
+}
 
 module.exports = {
   listUsers,
-  listUsersById
+  listUsersById,
+  createNewUser,
+  updateUser,
+  deleteUser
 }
--- routes/users.js
+++ routes/users.js
@@ -12,14 +12,14 @@ router.get('/users/:id', usersController.listUsersById)
 
 // // * POST /users
 // //   * Create a new user (sampleUser). Find a way to increment the id so that we always insert the next available id in the list. Currently we have users 1-10 (_data/index_). The next user should be 11
-// router.post('/users', usersController.createNewUser)
+router.post('/users', usersController.createNewUser)
 
 // // * PUT /users/:id
 // //   * Update one user matching the path param (id). You may again use the sampleUser code as your "body" for this request
-// router.put('/users/:id', usersController.updateUser)
+router.put('/users/:id', usersController.updateUser)
 
 // // * DELETE /users/:id
 // //   * Delete one user by it's id
-// router.delete('/users/:id', usersController.deleteUser)
+router.delete('/users/:id', usersController.deleteUser)
 
 module.exports = router
